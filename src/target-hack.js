28c6a4eaff3070bf4898a1ddab7c94414e1c2509
Tamilyn <tamilyn.peck2@extendhealth.com>
2022-08-23 18:29:34 -0600

    use ns.args

--- src/target-hack.js
+++ src/target-hack.js
@@ -2,9 +2,8 @@
 import { configureHack } from "configure-hack.js";
 
 export async function main(ns) {
-  const args_obj = arguments[0];
-  const target = args_obj.args[0] ? args_obj.args[0] : ns.getHostname();
-  const server = args_obj.args[1] ? args_obj.args[1] : null;
+  const target = ns.args[0] ? ns.args[0] : ns.getHostname();
+  const server = ns.args[1] ? ns.args[1] : null;
   ns.tprint(`Confiugre Hack on ${target} from ${server}`);
 
   await configureHack(ns, target, server);
